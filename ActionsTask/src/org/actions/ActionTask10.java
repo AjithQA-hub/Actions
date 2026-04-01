bd6a894cbfba5b6a3333721506ed23d437511217
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-01 13:09:58 +0530

    Update ActionTask10.java

--- ActionsTask/src/org/actions/ActionTask10.java
+++ ActionsTask/src/org/actions/ActionTask10.java
@@ -11,20 +11,15 @@ public class ActionTask10 {
 	public static void main(String[] args) throws InterruptedException {
         
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\InmakesActions\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://www.snapdeal.com/");
-		
 		driver.manage().window().maximize();
 		
 		Actions a=new Actions(driver);
 		
 		WebElement SnapDeal = driver.findElement(By.xpath("(//li[@class='navlink lnHeight'])[2]"));
 		a.moveToElement(SnapDeal).perform();
-		
 		Thread.sleep(3000);
-		
 		WebElement SnapDeal1 = driver.findElement(By.xpath(""));
 		SnapDeal1.click();
 	}
